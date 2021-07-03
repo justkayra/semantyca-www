15b7ff80bf050a1664b3282961d365e0348a5251
justa <justaidajam@gmail.com>
2021-07-03 19:31:15 +0300

    add links url

--- src/App.js
+++ src/App.js
@@ -5,14 +5,22 @@ function App() {
         <div>
             <header className="App-header">
                 <nav className="navbar navbar-expand-md navbar-dark navbar-custom">
-                    SEMANTYCA
+                    <a className="navbar-brand" href="https://www.semantyca.com">SEMANTYCA</a>
+
                     <button className="navbar-toggler" type="button" data-toggle="collapse"
                             data-target="#collapsibleNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                      afaf
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="https://www.semantyca.com">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="https://juka.semantyca.com">Tasks</a>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </header>
